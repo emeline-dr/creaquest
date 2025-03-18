1b008415570bcc29aea28962e364de57ab2d5e07
Emeline D.R. <deroo.emeline@gmail.com>
2025-03-18 12:05:31 +0100

    Modification 404

--- frontend/src/app/pages/no-page/no-page.component.ts
+++ frontend/src/app/pages/no-page/no-page.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
 import { TitleCreaquestComponent } from '../../components/title-creaquest/title-creaquest.component';
+import { CopyrightComponent } from '../../components/copyright/copyright.component';
 
 @Component({
   selector: 'app-no-page',
   standalone: true,
   imports: [
+    RouterModule,
     TitleCreaquestComponent,
-    RouterModule
+    CopyrightComponent
   ],
   templateUrl: './no-page.component.html',
   styleUrl: './no-page.component.css'
@@ -17,9 +21,12 @@ export class NoPageComponent {
   routeUrl = '/index';
   token: string | null = localStorage.getItem('token');
 
-  constructor() {
+  constructor(
+    private titleService: Title,
+  ) {
     if (!this.token) {
       this.routeUrl = '/home';
     }
+    this.titleService.setTitle("Créaquest - 404")
   }
 }
