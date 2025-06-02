c63975171b9fe5e866622a0bdb4412b633bf48fc
Emeline D.R. <deroo.emeline@gmail.com>
2025-06-02 20:01:44 +0200

    màj

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -7,7 +7,7 @@ import { AuthService } from './auth/auth.service';
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'http://localhost:8000/index.php?api=';
+  private apiUrl = 'http://emelinedr.alwaysdata.net/backend/api=';
   private apiRoute = '';
 
   constructor(
