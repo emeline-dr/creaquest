68a29b4b6156af668d363e95d660a4af7d930f69
Emeline D.R. <deroo.emeline@gmail.com>
2025-06-03 18:04:56 +0200

    test en commentant

--- backend/src/Database/DBConnexion.php
+++ backend/src/Database/DBConnexion.php
@@ -26,7 +26,7 @@ class DBConnexion
         return $this->pdo ?? $this->pdo = new PDO("mysql:dbname={$this->dbname};host={$this->host}", $this->username, $this->password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET UTF8'
+            /* PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET UTF8' */
         ]);
     }
 }
