5ff5354e56fdd169aad8f7dd6cf7c945bb78cf46
Emeline D.R. <deroo.emeline@gmail.com>
2025-06-25 17:27:39 +0200

    Correction

--- frontend/dist/creaquest-frontend/browser/chunk-RDQYOSHM.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-VSFXJAOG.js";import"./chunk-KTCSL446.js";import"./chunk-IJJY23WK.js";import"./chunk-GAL4ENT6.js";export{a as NoPageComponent};
---
+++ frontend/dist/creaquest-frontend/browser/chunk-TJABTSEN.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-R65AMFA3.js";import"./chunk-KCS3ILYB.js";import"./chunk-M52B5VA5.js";import"./chunk-GAL4ENT6.js";export{a as NoPageComponent};
